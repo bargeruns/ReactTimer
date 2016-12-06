09ff9e69b238767be5cc62cacd05ee9cad056c3f
Sean Brage <sean@layer3tv.com>
2016-12-06 13:19:13 -0700

    Finished testing and styling Countdown

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -17,7 +17,7 @@ describe('countdown', () => {
       countdown.handleSetCountdown(10);
 
       expect(countdown.state.count).toBe(10);
-      expect(countdown.state.countDownStatus).toBe('started');
+      expect(countdown.state.countdownStatus).toBe('started');
 
       setTimeout(() => {
         expect(countdown.state.count).toBe(9);
@@ -33,6 +33,30 @@ describe('countdown', () => {
         expect(countdown.state.count).toBe(0);
         done();
       }, 3001);
+    });
+
+    it('should stop counting while paused', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown />);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should stop count when stopped', (done) => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(()=> {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
     });
   });
 });
